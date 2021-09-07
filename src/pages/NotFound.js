63ae14647993bb5cc0d4368e28b33855d7de92dc
rahulbhadja <rahulbhadja@gmail.com>
2021-09-07 13:32:05 +0530

    first commit

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className='centered '>
+      <h4>Page not found!</h4>
+    </div>
+  );
+};
+
+export default NotFound;
